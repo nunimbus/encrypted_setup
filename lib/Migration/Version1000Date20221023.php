79289bdd565d3d4c9c90e945071953b4209eb3f9
summersab <andrew+github@platypusinnovations.com>
2022-11-13 19:27:26 -0600

    set `skeletondirectory` value; improve the uninstall steps

--- lib/Migration/Version1000Date20221023.php
+++ lib/Migration/Version1000Date20221023.php
@@ -46,7 +46,11 @@ class Version1000Date20221023 extends SimpleMigrationStep {
 		$server = OC::$server;
 
 		// This has to be in a Migration package because it runs prior to any apps loading (and thus, any files being created)
-		
+
+		// Set the `skeletondirectory` to empty - the encryption functions don't handle these files well durring install
+		$skeletonDir = $server->getConfig()->getSystemValue('skeletondirectory');
+		$server->getConfig()->setSystemValue('skeletondirectory', "");
+
 		// Enable the encryption app and load it
 		$server->getAppManager()->enableApp('encryption');
 		\OC_App::loadApp('encryption');
@@ -67,6 +71,14 @@ class Version1000Date20221023 extends SimpleMigrationStep {
 
 		// Disable the master key
 		$server->getConfig()->setAppValue('encryption', 'useMasterKey', '0');
+
+		// Clean up and remove the app
+		$server->getConfig()->setSystemValue('skeletondirectory', $skeletonDir);
+
+		$appId = OC::$server->get('OC\AppFramework\App')->getAppIdForClass(get_class($this));
+		$qb = OC::$server->get('OC\DB\QueryBuilder\QueryBuilder');
+		$qb->delete('migrations')->where($qb->expr()->eq('app', $qb->createNamedParameter($appId)));
+		$qb->execute();
 
 		$server->getAppManager()->disableApp('encrypted_setup');
 		$installer = $server->get('OC\Installer');
